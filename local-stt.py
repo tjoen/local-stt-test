efa1be1957a9d4dc458e55a0db622f4eb569583a
Theun Kohlbeck <t.kohlbeck@gmail.com>
2018-02-17 00:46:15 +0100

    Update local-stt.py

--- local-stt.py
+++ local-stt.py
@@ -1,10 +1,12 @@
+from ctypes import *
+from contextlib import contextmanager
 from os import environ, path
-
+import pyaudio
 from pocketsphinx.pocketsphinx import *
 from sphinxbase.sphinxbase import *
 
-
-# stop mycroft speech service
+# stop mycroft speech and audio service
+# sudo service mycroft-audio stop
 # sudo service mycroft-speech-client stop
 
 
@@ -16,8 +18,23 @@ config.set_string('-dict', path.join(HOMEDIR, 'localstt.dic'))
 config.set_string('-logfn', '/dev/null')
 decoder = Decoder(config)
 
-import pyaudio
-p = pyaudio.PyAudio()
+ERROR_HANDLER_FUNC = CFUNCTYPE(None, c_char_p, c_int, c_char_p, c_int, c_char_p)
+
+def py_error_handler(filename, line, function, err, fmt):
+    pass
+
+c_error_handler = ERROR_HANDLER_FUNC(py_error_handler)
+
+@contextmanager
+def noalsaerr():
+    asound = cdll.LoadLibrary('libasound.so')
+    asound.snd_lib_error_set_handler(c_error_handler)
+    yield
+    asound.snd_lib_error_set_handler(None)
+
+
+with noalsaerr():
+    p = pyaudio.PyAudio()
 stream = p.open(format=pyaudio.paInt16, channels=1, rate=16000, input=True, frames_per_buffer=1024)
 stream.start_stream()
 
@@ -32,7 +49,11 @@ while True:
             if not in_speech_bf:
                 decoder.end_utt()
                 print 'Result:', decoder.hyp().hypstr
+                utt = decoder.hyp().hypstr
                 decoder.start_utt()
+                if utt.strip() != '':
+                    print utt.strip()
+                    #break
     else:
         break
 decoder.end_utt()
